420739553488c8b99990c6ee9fa9fb5238770672
Sina Sajadmanesh <sina.sajadmanesh@sony.com>
2024-09-24 12:30:57 +0000

    generate with new corpus

---
+++ generate-jp-en.sh
@@ -0,0 +1,2 @@
+trdg -dt trdg/dicts/corpus_en_jp.txt -c 5000000 -w 1 -f 32 64 -t 64 -e jpg -k 15 -rk -bl 4 -rbl -b 0 -na 2 -d 4 -do 2 -or 2 -tc '#000000,#888888' -cs -4 16 -fd trdg/fonts/ja --output_dir /data/ocr/recog/jpensyn2/imgs/train
+trdg -dt trdg/dicts/corpus_en_jp.txt -c 5000 -w 1 -f 32 64 -t 64 -e jpg -k 15 -rk -bl 4 -rbl -b 0 -na 2 -d 4 -do 2 -or 2 -tc '#000000,#888888' -cs -4 16 -fd trdg/fonts/ja --output_dir /data/ocr/recog/jpensyn2/imgs/val
