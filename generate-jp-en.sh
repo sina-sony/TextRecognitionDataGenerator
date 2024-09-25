a687eff28ecc295729c48162260c0f5ad9e2cd67
Sina Sajadmanesh <sina.sajadmanesh@sony.com>
2024-09-25 11:55:45 +0000

    fix params

--- generate-jp-en.sh
+++ generate-jp-en.sh
@@ -16,9 +16,9 @@ ORIENT=2  # random orientation
 TEXT_COLOR='#000000,#888888'  # random color from 000000 to 888888
 MIN_CHAR_SPACE=-4
 MAX_CHAR_SPACE=16
-FONT_DIR=trdg/fonts/latin
-TRAIN_OUT_DIR=/data/ocr/recog/ensyn2/imgs/train
-VAL_OUT_DIR=/data/ocr/recog/ensyn2/imgs/val
+FONT_DIR=trdg/fonts/ja
+TRAIN_OUT_DIR=/data/ocr/recog/jpensyn2/imgs/train
+VAL_OUT_DIR=/data/ocr/recog/jpensyn2/imgs/val
 
 trdg -dt $CORPUS -c $TRAIN_COUNT -w $MAX_LEN -f $MIN_RES $MAX_RES -t $NUM_THREADS -e $FMT -k $MAX_SKEW -rk -bl $MAX_BLUR -rbl -b $BG_TYPE -na $NAME_FMT -d $DISTORT_TYPE -do $DISTORT_ORIENT -or $ORIENT -tc $TEXT_COLOR -cs $MIN_CHAR_SPACE $MAX_CHAR_SPACE -fd $FONT_DIR --output_dir $TRAIN_OUT_DIR
 trdg -dt $CORPUS -c $VAL_COUNT -w $MAX_LEN -f $MIN_RES $MAX_RES -t $NUM_THREADS -e $FMT -k $MAX_SKEW -rk -bl $MAX_BLUR -rbl -b $BG_TYPE -na $NAME_FMT -d $DISTORT_TYPE -do $DISTORT_ORIENT -or $ORIENT -tc $TEXT_COLOR -cs $MIN_CHAR_SPACE $MAX_CHAR_SPACE -fd $FONT_DIR --output_dir $VAL_OUT_DIR
