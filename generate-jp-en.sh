1bfbf2103e94ad3aced86283fa4cb72f1081f61d
Sina Sajadmanesh <sina.sajadmanesh@sony.com>
2024-09-25 11:39:31 +0000

    use variables in generation scripts

--- generate-jp-en.sh
+++ generate-jp-en.sh
@@ -1,2 +1,24 @@
-trdg -dt trdg/dicts/corpus_en_jp.txt -c 5000000 -w 1 -f 32 64 -t 64 -e jpg -k 15 -rk -bl 4 -rbl -b 0 -na 2 -d 4 -do 2 -or 2 -tc '#000000,#888888' -cs -4 16 -fd trdg/fonts/ja --output_dir /data/ocr/recog/jpensyn2/imgs/train
-trdg -dt trdg/dicts/corpus_en_jp.txt -c 5000 -w 1 -f 32 64 -t 64 -e jpg -k 15 -rk -bl 4 -rbl -b 0 -na 2 -d 4 -do 2 -or 2 -tc '#000000,#888888' -cs -4 16 -fd trdg/fonts/ja --output_dir /data/ocr/recog/jpensyn2/imgs/val
+CORPUS=trdg/dicts/corpus_en_jp.txt
+TRAIN_COUNT=5000000
+VAL_COUNT=5000
+MAX_LEN=3
+MIN_RES=32
+MAX_RES=64
+NUM_THREADS=64
+FMT=jpg
+MAX_SKEW=15
+MAX_BLUR=4
+BG_TYPE=0  # gaussian noise
+NAME_FMT=2  # [ID].[EXT] + one file labels.txt containing id-to-label mappings
+DISTORT_TYPE=4  # random distortion
+DISTORT_ORIENT=2  # random orientation
+ORIENT=2  # random orientation
+TEXT_COLOR='#000000,#888888'  # random color from 000000 to 888888
+MIN_CHAR_SPACE=-4
+MAX_CHAR_SPACE=16
+FONT_DIR=trdg/fonts/latin
+TRAIN_OUT_DIR=/data/ocr/recog/ensyn2/imgs/train
+VAL_OUT_DIR=/data/ocr/recog/ensyn2/imgs/val
+
+trdg -dt $CORPUS -c $TRAIN_COUNT -w $MAX_LEN -f $MIN_RES $MAX_RES -t $NUM_THREADS -e $FMT -k $MAX_SKEW -rk -bl $MAX_BLUR -rbl -b $BG_TYPE -na $NAME_FMT -d $DISTORT_TYPE -do $DISTORT_ORIENT -or $ORIENT -tc $TEXT_COLOR -cs $MIN_CHAR_SPACE $MAX_CHAR_SPACE -fd $FONT_DIR --output_dir $TRAIN_OUT_DIR
+trdg -dt $CORPUS -c $VAL_COUNT -w $MAX_LEN -f $MIN_RES $MAX_RES -t $NUM_THREADS -e $FMT -k $MAX_SKEW -rk -bl $MAX_BLUR -rbl -b $BG_TYPE -na $NAME_FMT -d $DISTORT_TYPE -do $DISTORT_ORIENT -or $ORIENT -tc $TEXT_COLOR -cs $MIN_CHAR_SPACE $MAX_CHAR_SPACE -fd $FONT_DIR --output_dir $VAL_OUT_DIR
